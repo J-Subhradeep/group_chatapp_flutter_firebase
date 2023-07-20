44af3cb7a9c2dc945ab72d5292230339b304b481
Subhradeep <subhradeeppal30@gmail.com>
2023-07-20 21:08:39 +0530

    git commit first

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:936144704550:web:a96abbd8d55c5ed2e5c2e7";
+  static String apiKey = "AIzaSyAxNl6OUgdHNbvUTWh5fIblb1qDn5hmk8c";
+  static String messagingSenderId = "936144704550";
+  static String projectId = "chatappflutterfirebase-c3b99";
+  final primaryColor = const Color(0xFFee7b64);
+}
